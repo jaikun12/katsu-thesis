05aa3a844eb25c087f079cb7208efade5742d16b
boazcstrike <boazcstrike@yahoo.com.sg>
2017-03-05 01:03:03 +0800

    Overwritten katsudb

--- SQL/katsudb.sql
+++ SQL/katsudb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 20, 2017 at 11:55 AM
+-- Generation Time: Mar 04, 2017 at 06:02 PM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -30,9 +30,33 @@ CREATE TABLE `answers_table` (
   `answer_id` int(15) NOT NULL,
   `question_id` int(15) NOT NULL,
   `victim_id` int(15) NOT NULL,
+  `user_id` int(15) NOT NULL,
   `answer_content` varchar(1000) NOT NULL,
   `date_answered` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP
 ) ;
+
+--
+-- Dumping data for table `answers_table`
+--
+
+INSERT INTO `answers_table` (`answer_id`, `question_id`, `victim_id`, `user_id`, `answer_content`, `date_answered`) VALUES
+(1, 1, 1, 1, 'yes', '2017-03-03 04:41:01.443838'),
+(2, 2, 1, 1, 'yes', '2017-03-03 04:41:01.519948'),
+(3, 3, 1, 1, 'yes', '2017-03-03 04:41:01.522645'),
+(4, 4, 1, 1, 'yes', '2017-03-03 04:41:01.525118'),
+(5, 5, 1, 1, 'yes', '2017-03-03 04:41:01.527557'),
+(6, 1, 2, 1, 'yes', '2017-03-03 04:41:01.530090'),
+(7, 2, 2, 1, 'yes', '2017-03-03 04:41:01.532723'),
+(8, 3, 2, 1, 'no', '2017-03-03 04:41:01.534380'),
+(9, 4, 2, 1, 'yes', '2017-03-03 04:41:01.535799'),
+(10, 5, 2, 1, 'no', '2017-03-03 04:41:01.572763'),
+(11, 5, 2, 1, 'no', '2017-03-03 04:41:01.574595'),
+(12, 1, 1, 18, 'no', '2017-03-03 04:59:16.933405'),
+(13, 2, 1, 18, 'no', '2017-03-03 04:59:19.168304'),
+(14, 3, 1, 18, 'no', '2017-03-03 05:06:40.815260'),
+(15, 4, 1, 18, 'no', '2017-03-03 05:06:42.379016'),
+(16, 5, 1, 18, 'no', '2017-03-03 05:06:43.963172'),
+(17, 1, 5, 18, 'yes', '2017-03-04 04:54:00.374491');
 
 -- --------------------------------------------------------
 
@@ -50,8 +74,20 @@ CREATE TABLE `childs_table` (
   `child_gender` varchar(45) NOT NULL,
   `child_prov` varchar(45) NOT NULL,
   `child_city` varchar(45) NOT NULL,
-  `child_pword` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `child_pword` varchar(45) NOT NULL,
+  `time_created` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP
+) ;
+
+--
+-- Dumping data for table `childs_table`
+--
+
+INSERT INTO `childs_table` (`child_id`, `user_id`, `child_fname`, `child_mname`, `child_lname`, `child_age`, `child_gender`, `child_prov`, `child_city`, `child_pword`, `time_created`) VALUES
+(1, 18, 'Boaz Michael', 'Yutatco', 'Sze', 8, 'male', 'Manila', 'Manila', '$!zhnR4SAWYhE', '2017-03-03 05:26:10.808095'),
+(2, 18, 'Kristine', 'Ancheta', 'Aquino', 10, 'female', 'Laguna', 'Calamba', '$!zhnR4SAWYhE', '2017-03-03 03:06:36.199776'),
+(3, 18, 'John', 'Alvarez', 'Lim', 8, 'male', 'Cavite', 'Dasmarinas', '$!zhnR4SAWYhE', '2017-03-03 03:09:38.573788'),
+(4, 18, 'John Robert', 'Tiongson', 'Sevilla', 15, 'male', 'Nueva Ecija', 'Sullano', '$!zhnR4SAWYhE', '2017-03-03 03:13:08.688437'),
+(5, 18, 'Jhoebe', 'Kahit Ano', 'Lim', 8, 'female', 'Manila', 'Manila', '$!zhnR4SAWYhE', '2017-03-04 04:53:15.767916');
 
 -- --------------------------------------------------------
 
@@ -75,8 +111,6 @@ CREATE TABLE `questions_table` (
   `question_id` int(15) NOT NULL,
   `user_id` int(15) NOT NULL,
   `question_content` varchar(1000) NOT NULL,
-  `question_weight` varchar(100) NOT NULL,
-  `question_category` varchar(50) NOT NULL,
   `date_created` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP
 ) ;
 
@@ -84,12 +118,12 @@ CREATE TABLE `questions_table` (
 -- Dumping data for table `questions_table`
 --
 
-INSERT INTO `questions_table` (`question_id`, `user_id`, `question_content`, `question_weight`, `question_category`, `date_created`) VALUES
-(1, 0, 'Gumagamit ka ba ng computer?', 'neutral_question', 'general', '2017-02-20 10:52:10.373414'),
-(2, 0, 'Meron ba kayong internet sa bahay?', 'neutral_question', 'general', '2017-02-20 10:52:10.375868'),
-(3, 0, 'May humawak na ba sa maselang bahagi ng katawan mo maliban sa magulang mo?', 'heavy_negative', 'child abuse', '2017-02-20 10:54:53.922208'),
-(4, 0, 'Alam mo ba itsura ng webcam?', 'neutral_question', 'child porn', '2017-02-20 10:52:10.380058'),
-(5, 1, 'May nagpahubad na ba sayo sa harap ng webcam', 'heavy_negative', 'child porn', '2017-02-20 10:52:10.382261');
+INSERT INTO `questions_table` (`question_id`, `user_id`, `question_content`, `date_created`) VALUES
+(1, 0, 'Gumagamit ka ba ng computer?', '2017-02-20 10:52:10.373414'),
+(2, 0, 'Meron ba kayong internet sa bahay?', '2017-02-20 10:52:10.375868'),
+(3, 0, 'May humawak na ba sa maselang bahagi ng katawan mo maliban sa magulang mo?', '2017-02-20 10:54:53.922208'),
+(4, 0, 'Alam mo ba itsura ng webcam?', '2017-02-20 10:52:10.380058'),
+(5, 1, 'May nagpahubad na ba sayo sa harap ng webcam', '2017-02-20 10:52:10.382261');
 
 -- --------------------------------------------------------
 
@@ -100,7 +134,7 @@ INSERT INTO `questions_table` (`question_id`, `user_id`, `question_content`, `qu
 CREATE TABLE `users_table` (
   `user_id` int(15) NOT NULL,
   `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `password` varchar(75) NOT NULL,
   `is_admin` int(2) NOT NULL,
   `firstname` varchar(45) NOT NULL,
   `middlename` varchar(45) DEFAULT NULL,
@@ -115,23 +149,11 @@ CREATE TABLE `users_table` (
 -- Dumping data for table `users_table`
 --
 
-INSERT INTO `users_table` (`user_id`, `username`, `password`, `is_admin`, `firstname`, `middlename`, `lastname`, `contact_num`, `email`, `is_active`, `time_created`) VALUES
-(1, 'katsu_admin', 'admin', 1, 'Kim', 'Bok', 'Joo', '2147483647', 'sample@gmail.com', 1, '2017-02-20 09:00:36.395225'),
-(2, 'katsu_user', 'user', 1, 'Song', 'Joong', 'Ki', '2147483647', 'sample@gmail.com', 1, '2017-02-20 09:00:36.395225'),
-(9, 'katsu_admin1', '1234', 0, 'Rap', 'Sample Middle Name', 'Sev', '0', '', 0, '2017-02-20 10:08:49.031614'),
-(10, 'katsu_admin2', '1234', 0, 'Rap', 'Sample Middle Name', 'Sev', '2147483647', 'sample@gmail.com', 0, '2017-02-20 10:10:06.551580'),
-(11, 'katsu_admin3', '1234', 0, 'Rap', 'Sample Middle Name', 'Sev', '2147483647', 'sample@gmail.com', 0, '2017-02-20 10:11:39.079962'),
-(12, 'katsu_admin4', '1234', 0, 'Rap', 'Sample Middle Name', 'Sev', '9175512388', 'sample@gmail.com', 0, '2017-02-20 10:13:40.165680');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `childs_table`
---
-ALTER TABLE `childs_table`
-  ADD PRIMARY KEY (`child_id`);
+INSERT INTO `users_table` (`user_id`, `username`, `password`, `is_admin`, `firstname`, `middlename`, `lastname`, `contact_num`, `email`, `is_active`, `time_created`, `created_by`) VALUES
+(17, 'katsu_admin', '!@Fugcg161Fy6', 1, 'Bok Joo', 'NA', 'Kim', '123456789', 'sample@gmail.com', 1, '2017-03-03 02:38:54.930030', 'super admin'),
+(18, 'katsu_user', '!@J4Q/0zUJamw', 0, 'Shi Jin', 'NA', 'Yoo', '091234567', 'sample@gmail.com', 1, '2017-03-03 02:38:54.976249', 'super admin'),
+(19, 'katsu_inactive', '!@D5mroWvawd6', 1, 'Shi Jin', 'NA', 'Yoo', '092323231', 'sample@gmail.com', 0, '2017-03-03 02:38:54.993716', 'super admin'),
+(20, 'boazcstrike', '$!RwKbHxrwnPw', 1, 'Boaz Sze', 'Yutatco', 'Sze', '09171231234', 'sample@gmail.com', 1, '2017-03-03 05:15:33.832610', '');
 
 --
 -- AUTO_INCREMENT for dumped tables
