5eb3a05f9f00e46595b191ca3c3a78c01d66a57b
boazcstrike <boazcstrike@yahoo.com.sg>
2017-03-06 19:07:15 +0800

    SQL update

--- SQL/katsudb030617_1902.sql
+++ SQL/katsudb030617_1902.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 06, 2017 at 12:04 PM
+-- Generation Time: Mar 06, 2017 at 12:06 PM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
