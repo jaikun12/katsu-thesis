adc5adb93ac387df28cac205b97519e60d4df001
boazcstrike <boazcstrike@yahoo.com.sg>
2017-03-06 10:41:58 +0800

    Added fixed css to katsu-chat

--- katsu-chat.php
+++ katsu-chat.php
@@ -1,7 +1,7 @@
 <?php
 	require_once("php/dbconnect.php");
 	require("php/session/session_check.php");
-	include("partial_view/essentials-upper-user.html");
+	include("partial_view/essentials-upper-chat.html");
 	include("partial_view/essentials-lower.html");
 
 	$question_no = $_GET['question'];
@@ -25,53 +25,7 @@
 			<center>
 
 		<?php
-		if(isset($_GET['question'])){
-			$check_end = $_GET['question'];
-		}
-
-		if($check_end == "end"){
-
-			echo '
-				<img id="katsu" src="images/katsu-end.gif">
-
-				<h2>Salamat sa usapan natin. Sa uulitin ';
-
-			$get_child = mysqli_query($connection, "SELECT * FROM childs_table WHERE child_id = $child_id;");
-
-			if($r=mysqli_fetch_array($get_child)){
-				$fname = $r['child_fname'];
-			}
-
-			echo $fname . '! <span class="glyphicon glyphicon-heart" aria-hidden="true"></span></h2>
-			<h4>Pakibalik na po ako kay ate o kuya.</h4>
-			<br><br><br><br>
-
-			<a href="katsu-start.php" class="primary-btn">End session</a>
-		
-			</div>
-
-			';
-			unset()
-
-		}else{
-			echo '
-			<img id="katsu" src="images/katsu.gif">
-				<h2>' . $question . '</h2>
-				
-				<form action="php/submit_response.php" method="POST">
-					<button type="submit" name="answer" value="yes">Oo</button>
-					<button type="submit" name="answer" value="no">Hindi</button>
-					<input type="text" name="question_no" value="' . $question_no . '" style="display:none;">
-				</form>
-				</center>
-
-			</div>
-
-			';
-		}
-
-		
-
+			include("php/chat_output.php");
 		?>
 
 	</body>
---
+++ php/chat_output.php
@@ -0,0 +1,46 @@
+<?php
+	if(isset($_GET['question'])){
+			$check_end = $_GET['question'];
+		}
+
+		if($check_end == "end"){
+
+			echo '
+				<img id="katsu" src="images/katsu-end.gif">
+
+				<h2>Salamat sa usapan natin. Sa uulitin ';
+
+			$get_child = mysqli_query($connection, "SELECT * FROM childs_table WHERE child_id = $child_id;");
+
+			if($r=mysqli_fetch_array($get_child)){
+				$fname = $r['child_fname'];
+			}
+
+			echo $fname . '! <span class="glyphicon glyphicon-heart" aria-hidden="true"></span></h2>
+			<h4>Pakibalik na po ako kay ate o kuya.</h4>
+			<br><br><br><br>
+
+			<a href="katsu-start.php" class="primary-btn">End session</a>
+		
+			</div>
+
+			';
+
+		}else{
+			echo '
+			<img id="katsu" src="images/katsu.gif">
+				<h2>' . $question . '</h2>
+				
+				<form action="php/submit_response.php" method="POST">
+					<button type="submit" name="answer" value="yes">Oo</button>
+					<button type="submit" name="answer" value="no">Hindi</button>
+					<input type="text" name="question_no" value="' . $question_no . '" style="display:none;">
+				</form>
+				</center>
+
+			</div>
+
+			';
+		}
+
+		?>
